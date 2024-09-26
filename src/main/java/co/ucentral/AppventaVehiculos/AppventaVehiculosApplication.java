92a5a14684c653166fe10f0ea6413a0292496d28
Juan Oicata <jeotoicata@gmail.com>
2024-09-26 18:48:24 -0500

    nuevo: (primer commit)

---
+++ src/main/java/co/ucentral/AppventaVehiculos/AppventaVehiculosApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.AppventaVehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppventaVehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppventaVehiculosApplication.class, args);
+	}
+
+}
